ca1f8123a4166bc819a5f7fd0dc63e4a2a058e4f
Alexandrass33 <144776805+Alexandrass33@users.noreply.github.com>
2023-09-20 23:07:22 +0300

    Add files via upload

---
+++ script/bag.js
@@ -0,0 +1,5 @@
+var k=0;
+function count() {
+    k=k+1;
+    document.getElementById("count").innerHTML=k;
+}
---
+++ script/modal.js
@@ -0,0 +1,7 @@
+function show() {
+    document.getElementById("modalback").style.display="block"
+    document.getElementById("modalback").class="show"
+}
+function hide() {
+    document.getElementById("modalback").style.display="none"
+}
